8747db984a66e73cb19038ead8ebadec42e92baf
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2018-09-03 17:44:17 -0700

    Setting up travis (attempt 1)

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CalendarControllerTest < ActionDispatch::IntegrationTest
   test "should get get" do
-    get calendar_get_url
+    get calendar_path
     assert_response :success
   end
 
