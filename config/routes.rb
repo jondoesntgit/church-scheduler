1cfd8a06d4de40584f90342b916da654add1c7b9
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2018-09-04 21:41:01 -0700

    Added 'calendar' as app root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'calendar' => 'calendar#index'
+
+  root 'calendar#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
